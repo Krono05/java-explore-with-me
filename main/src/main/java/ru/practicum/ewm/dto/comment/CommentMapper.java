94beb0bdcf7c7988a165771b7944e444ba7d8f75
Тамир Болданов <rpp-2011@yandex.ru>
2024-03-08 08:37:30 +0300

    feature-comments fix-review

--- main/src/main/java/ru/practicum/ewm/dto/comment/CommentMapper.java
+++ main/src/main/java/ru/practicum/ewm/dto/comment/CommentMapper.java
@@ -1,9 +1,11 @@
 package ru.practicum.ewm.dto.comment;
 
-import ru.practicum.ewm.model.comment.Comment;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
+import ru.practicum.ewm.model.comment.Comment;
+
+import static ru.practicum.ewm.util.Constants.DATE_FORMAT;
 
 @Mapper
 public interface CommentMapper {
@@ -13,7 +15,9 @@ public interface CommentMapper {
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "author", ignore = true)
     @Mapping(target = "created", ignore = true)
+    @Mapping(target = "event", ignore = true)
     Comment toComment(NewCommentDto newCommentDto);
 
     CommentDto toCommentDto(Comment comment);
+
 }
