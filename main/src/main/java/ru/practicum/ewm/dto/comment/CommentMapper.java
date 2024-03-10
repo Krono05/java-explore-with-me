9c1df086d7b59e708503467710bfd4ba7e49f239
Тамир Болданов <rpp-2011@yandex.ru>
2024-03-10 20:12:57 +0300

    feature-comments fix-review

--- main/src/main/java/ru/practicum/ewm/dto/comment/CommentMapper.java
+++ main/src/main/java/ru/practicum/ewm/dto/comment/CommentMapper.java
@@ -13,26 +13,3 @@ public interface CommentMapper {
 
     CommentDto toCommentDto(Comment comment);
 }
-////
-//    public static Comment toComment(NewCommentDto newCommentDto) {
-//        return new Comment(
-//                null,
-//                newCommentDto.getText(),
-//                null,
-//                null,
-//                null,
-//                newCommentDto.getRating()
-//
-//        );
-//    }
-//
-//    public static CommentDto toCommentDto(Comment comment) {
-//        return new CommentDto(
-//                comment.getId(),
-//                comment.getAuthor().getName(),
-//                comment.getText(),
-//                comment.getCreated().format(DATE_FORMAT),
-//                comment.getRating()
-//        );
-//    }
-//}
