99db36fc3eae4fa7da4c92a0aa60db615838d085
Тамир Болданов <rpp-2011@yandex.ru>
2024-03-10 19:57:11 +0300

    feature-comments fix-review

--- main/src/main/java/ru/practicum/ewm/dto/comment/CommentMapper.java
+++ main/src/main/java/ru/practicum/ewm/dto/comment/CommentMapper.java
@@ -1,20 +1,14 @@
 package ru.practicum.ewm.dto.comment;
 
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.factory.Mappers;
 import ru.practicum.ewm.model.comment.Comment;
 
-@Mapper(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+@Mapper
 public interface CommentMapper {
 
     CommentMapper INSTANCE = Mappers.getMapper(CommentMapper.class);
 
-//    @Mapping(target = "id", ignore = true)
-//    @Mapping(target = "author", ignore = true)
-//    @Mapping(target = "created", ignore = true)
-//    @Mapping(target = "event", ignore = true)
     Comment toComment(NewCommentDto newCommentDto);
 
     CommentDto toCommentDto(Comment comment);
